c79b969b38ab5b6da481f459dff45391ce1a80be
bgitego <bgitego@gmail.com>
2018-04-19 22:30:09 -0400

    Updated Comment field

--- BaseConv.c
+++ BaseConv.c
@@ -1,13 +1,15 @@
 #include "BaseConv.h"
 #include "Common.h"
 
+/*This function takes 16 bit Unsigned array holding 12bit data then, convert and store the data as 16 bit signed*/
+
 void twelveToSixten(U16 *raw_data, S16 *converted_data, U16 num_of_elements)
 {
     S16 converted_word;
 
     for(U16 i = 0; i < num_of_elements; i++)
     {
-        //take 12 bit word, shift it by 4 bit to the left, then divide by 4 this should convert to 
+        //Shift 12 bit word by 4 bit to the left, then divide by 4 to set the MSB if value is negative. 
         converted_word = (S16) (*(raw_data+i) << 4 )/(1<<4);
        *(converted_data+i) = converted_word;
     }
--- BaseConv.h
+++ BaseConv.h
@@ -9,5 +9,6 @@
 // stores converted 12 to 16 bit signed data.
 S16 conv_record_buffer[1024];
 
+
 void twelveToSixten(U16 *,S16 *, U16 );
 #endif
